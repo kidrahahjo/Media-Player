879c3c196e7c35ec6b733f36359742c7b2ce3e45
kidrahahjo <kidrahahjo@gmail.com>
2019-10-28 23:22:43 +0530

    Service Initiated

---
+++ app/src/main/java/com/i/mediaplayer/musicService.java
@@ -0,0 +1,31 @@
+package com.i.mediaplayer;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+public class musicService extends Service{
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+}
